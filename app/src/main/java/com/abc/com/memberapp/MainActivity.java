15f45bd4929bef9810f42afe5fa8fe1c30b5a0e9
xornjseh1000@gmail.com <xornjseh1000@gmail.com>
2016-07-27 15:37:53 +0900

    member start

--- app/src/main/java/com/abc/com/memberapp/MainActivity.java
+++ app/src/main/java/com/abc/com/memberapp/MainActivity.java
@@ -1,13 +1,37 @@
 package com.abc.com.memberapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity implements View.OnClickListener {
+    EditText et_id,et_pw;
+    Button bt_login,bt_join;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et_id = (EditText) findViewById(R.id.et_id);
+        et_pw = (EditText) findViewById(R.id.et_pw);
+        bt_login = (Button) findViewById(R.id.bt_login);
+        bt_join = (Button) findViewById(R.id.bt_join);
+        bt_login.setOnClickListener(this);
+        bt_join.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bt_join:
+
+                break;
+            case R.id.bt_login:
+                Toast.makeText(MainActivity.this,"ID:"+et_id.getText().toString()
+                        +"PW:"+et_pw.getText().toString(),Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 }
